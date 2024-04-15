50dd13bd6360e868b740cbfebeaa059d683278b2
Nemanja Ilic <155994048+Nemanja264@users.noreply.github.com>
2024-04-15 15:10:52 +0200

    Create main.cpp

---
+++ LList/jun_2022/main.cpp
@@ -0,0 +1,24 @@
+//Proba commita
+#include "LList.h"
+
+int main()
+{
+	LList *list = new LList();
+	list->addToHead(3);
+	list->addToHead(3);
+	list->addToHead(6);
+	list->addToHead(4);
+	list->addToHead(2);
+	list->addToHead(7);
+	list->addToHead(6);
+	list->addToHead(1);
+	list->addToHead(9);
+	list->addToHead(7);
+	list->addToHead(4);
+	
+	list->GroupElements();
+	list->printAll();
+
+	delete list;
+	return 0;
+}
